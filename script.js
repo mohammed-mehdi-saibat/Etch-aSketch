f52e844cd7b12a1ec829426f3b9c6ca13e2703ce
saibat01 <mohammedmehdisaibat@gmail.com>
2025-06-06 18:47:09 +0100

    Implement hover effect to change square background color on mouseover

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function createGrid(size = 16) {
 
   container.innerHTML = "";
 
-  const squareSize = 960 / size;
+  const squareSize = Math.floor(960 / size);
 
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = `${100 / size}%`;
+    square.style.height = `${100 / size}%`;
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "yellow";
+    });
+
     container.appendChild(square);
   }
 }
